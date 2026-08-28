fe127c2b6858861d7661f466767a3e264fd63596
Yhormungandr <makironien@live.se>
2026-08-28 10:46:39 +0200

    vi ser om det smäller

--- Fredagsgodis/Fredagsgodiset/Program.cs
+++ Fredagsgodis/Fredagsgodiset/Program.cs
@@ -1,1 +1,15 @@
-﻿internal class Program
+﻿using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+class program
+{
+
+    static void Main(string[] args)
+    {
+        string filePath = Path.Combine(
+        AppContext.BaseDirectory,
+        "data",
+        "Countries_area.txt"
+        );
+
+    }
+}
